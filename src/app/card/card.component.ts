447c066b77ec567945202882ab3c81436480c2ad
User1 <user1@Skycliffitdomain.com>
2019-09-04 14:11:39 +0530

    changes

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
 import { Patdetails } from './patdetails';
+import { Category } from './demo';
+import { INT_TYPE } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-card',
@@ -10,112 +12,114 @@ import { Patdetails } from './patdetails';
 })
 export class CardComponent implements OnInit {
 
-
-  details:FormGroup;
-  callerdetails:FormGroup;
+  details: FormGroup;
+  callerdetails: FormGroup;
   general: FormGroup;
   address: FormGroup;
   contact: FormGroup;
   other: FormGroup;
 
-  isActive:Boolean=false;
+  isActive: Boolean = false;
 
   closeResult: string;
   constructor(private modalService: NgbModal, private fb: FormBuilder) { }
 
-  fisrtname:string='';
-  lastname:string='';
-  phone:string='';
-  email:string='';
-  arr:Patdetails[]=[
-    new Patdetails('ak','zz','12365','sdgvdv'),
-    new Patdetails('ak','zz','12365','sdgvdv'),
-    new Patdetails('aks','zz','12365','sdgvdv'),
-    new Patdetails('ak','zz','12365','sdgvdv'),
-    new Patdetails('bs','zz','12365','sdgvdv'),
-    new Patdetails('nm','zz','12365','sdgvdv'),
-    new Patdetails('re','zz','12365','sdgvdv'),
-    new Patdetails('yt','zz','12365','sdgvdv'),
-    new Patdetails('jh','zz','12365','sdgvdv')
+  patname: string = '';
+  fisrtname: string = '';
+  lastname: string = '';
+  phone: string = '';
+  email: string = '';
+  arr: Patdetails[] = [
+    new Patdetails('bs', 'zz', '12365', 'sdgvdv'),
+    new Patdetails('ak', 'zz', '12365', 'sdgvdv'),
+    new Patdetails('ak', 'zz', '12365', 'sdgvdv'),
+    new Patdetails('ak', 'zz', '12365', 'sdgvdv'),
+    new Patdetails('bs', 'zz', '12365', 'sdgvdv'),
+    new Patdetails('nm', 'zz', '12365', 'sdgvdv'),
+    new Patdetails('re', 'zz', '12365', 'sdgvdv'),
+    new Patdetails('yt', 'zz', '12365', 'sdgvdv'),
+    new Patdetails('jh', 'zz', '12365', 'sdgvdv')
   ];
 
   ngOnInit() {
 
-    this.details=this.fb.group({
-      patientname:new FormControl(null),
-      patientDOB:new FormControl(null),
-      phone:new FormControl(null),
-      email:new FormControl(null)
+    this.details = this.fb.group({
+      patientname: new FormControl(null),
+      patientDOB: new FormControl(null),
+      phone: new FormControl(null),
+      email: new FormControl(null)
     });
 
-    this.callerdetails=this.fb.group({
-      callername:new FormControl(null),
-      phoneno:new FormControl(null)
+    this.callerdetails = this.fb.group({
+      callername: new FormControl(null),
+      phoneno: new FormControl(null)
     });
 
-    this.general=this.fb.group({
-      firstname:new FormControl(null),
-      middlename:new FormControl(null),
-      lastname:new FormControl(null),
-      dob:new FormControl(null),
+    this.general = this.fb.group({
+      firstname: new FormControl(null),
+      middlename: new FormControl(null),
+      lastname: new FormControl(null),
+      dob: new FormControl(null),
       gender: new FormControl(null)
     });
 
-    this.address=this.fb.group({
-      address1:new FormControl(null),
-      address2:new FormControl(null),
-      zip:new FormControl(null),
-      city:new FormControl(null),
-      state:new FormControl(null)
+    this.address = this.fb.group({
+      address1: new FormControl(null),
+      address2: new FormControl(null),
+      zip: new FormControl(null),
+      city: new FormControl(null),
+      state: new FormControl(null)
     });
 
-    this.contact=this.fb.group({
-      home:new FormControl(null),
-      cell:new FormControl(null),
-      workphone:new FormControl(null),
-      email:new FormControl(null),
-      mode:new FormControl(null)
+    this.contact = this.fb.group({
+      home: new FormControl(null),
+      cell: new FormControl(null),
+      workphone: new FormControl(null),
+      email: new FormControl(null),
+      mode: new FormControl(null)
     });
 
-    this.other=this.fb.group({
-      ssn:new FormControl(null),
-      language:new FormControl(null)
+    this.other = this.fb.group({
+      ssn: new FormControl(null),
+      language: new FormControl(null)
     });
 
   }
 
-  openEdit(content, i) {
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
-  }
+  openform(reg, content1) {
 
-  openDisplay(content1, i) {
-    this.modalService.open(content1, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
-  }
+    let pn = this.details.get('patientname').value;
+    console.log(pn);
 
-  // modal
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return  `with: ${reason}`;
+    for (let i = 0; i < this.arr.length; i++) {
+      let pname = this.arr[i].patname;
+      console.log(this.arr.length);
+      console.log(pname);
+      if (pname === pn) {
+        console.log('pname');
+        this.modalService.open(content1, { size: 'xl' });
+
+      }
+      //  if(this.details.value.patientname.value === this.patname){
+      //     console.log('pname');
+      //     this.modalService.open(content1, { size: 'xl' });
+      //          }
+      else {
+        console.log(pn);
+
+        this.general.patchValue({
+          firstname: this.details.value.patientname
+        });
+
+        this.modalService.open(reg, { size: 'xl' });
+      }
     }
   }
 
-  onCheck()
-  {
-    this.isActive=true;
+  onCheck() {
+    this.isActive = true;
   }
 
-  }
+}
 
 
--- src/app/card/patdetails.ts
+++ src/app/card/patdetails.ts
@@ -1,6 +1,6 @@
 export class Patdetails {
   public constructor(
-    public firstname:string,
+    public patname:string,
     public lastname:string,
     public phone:string,
     public email:string
